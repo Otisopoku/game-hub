3c8a8678f723bb85272001cab0074dfa87f67f64
Otis Opoku <otisopoku30@gmail.com>
2025-08-29 00:52:20 +0000

    add tooltip to display hidden description on hover

--- src/components/created_components/MovieCard.tsx
+++ src/components/created_components/MovieCard.tsx
@@ -1,5 +1,6 @@
 import { Movie } from "@/hooks/useMovies"
 import { Button, Card, Image, Text } from "@chakra-ui/react"
+import { Tooltip } from "@/components/ui/tooltip";
 
 interface Props{
     movie: Movie
@@ -9,7 +10,7 @@ const descriptionLimit = 200;
 
 const shortenDescription = (description: string) => {
     if (description.length > descriptionLimit){
-        return description.slice(0, descriptionLimit) + "....";
+        return description.slice(0, descriptionLimit);
     }
     return description;
 
@@ -22,12 +23,25 @@ const getBackdropUrl = (backDropPath: string) => {
 }
 const MovieCard = ({movie}: Props) => {
     const image_src = getBackdropUrl(movie.backdrop_path);
+    const isLong = movie.overview.length > descriptionLimit;
     return (
         <Card.Root maxW = "sm" overflow="hidden" borderRadius="10px">
             <Image src={image_src} width="100%"/>
             <Card.Body>
                 <Card.Title>{movie.title}</Card.Title>
-                <Text>{shortenDescription(movie.overview)}</Text>
+
+                {
+                    isLong ? (
+                        <Text>
+                            {shortenDescription(movie.overview)}
+                            <Tooltip content={movie.overview} positioning = {{placement: "top"}}>
+                                <Button size = "xs" variant="ghost" p={1} ><Text fontSize="2xl">💡</Text></Button>
+                            </Tooltip>
+                        </Text>
+                    ): (
+                        <Text>{movie.overview}</Text>
+                    )
+                }
             </Card.Body>
             <Card.Footer>
                 <Button>{`Rating: ${movie.vote_average}`}</Button>
