f2e7601f887407c2d5eaef201c583e3bd4734ed6
Otis Opoku <otisopoku30@gmail.com>
2025-08-31 02:18:41 +0000

    implement hover animation

--- src/components/created_components/MovieCard.tsx
+++ src/components/created_components/MovieCard.tsx
@@ -1,6 +1,9 @@
+
+import {motion} from "motion/react"
 import { Movie } from "@/hooks/useMovies"
 import { Button, Card, Image, Text } from "@chakra-ui/react"
 import { Tooltip } from "@/components/ui/tooltip";
+import { SiPrimevideo } from "react-icons/si";
 
 interface Props{
     movie: Movie
@@ -21,11 +24,33 @@ const imageBaseUrl = "https://image.tmdb.org/t/p/w300";
 const getBackdropUrl = (backDropPath: string) => {
     return `${imageBaseUrl}${backDropPath}`
 }
+
+const MotionCard = motion.create(Card.Root);
+
 const MovieCard = ({movie}: Props) => {
     const image_src = getBackdropUrl(movie.backdrop_path);
     const isLong = movie.overview.length > descriptionLimit;
     return (
-        <Card.Root maxW = "sm" overflow="hidden" borderRadius="10px">
+        
+        <MotionCard
+            maxW = "2xl"
+            overflow="hidden"
+            borderRadius= "4xl"
+            boxShadow="md"
+            whileHover={{
+                scale: 1.1,
+                transition: { duration: 0.2 },
+                zIndex: 1
+            }}
+            whileTap={{
+                scale: 0.8,
+            }}
+            transition={{
+                type: "spring",
+                stiffness: 400,
+                damping: 20,
+            }}
+            >
             <Image src={image_src} width="100%"/>
             <Card.Body>
                 <Card.Title>{movie.title}</Card.Title>
@@ -42,14 +67,18 @@ const MovieCard = ({movie}: Props) => {
                         <Text>{movie.overview}</Text>
                     )
                 }
+                <SiPrimevideo size="50px"/>
             </Card.Body>
             <Card.Footer>
                 <Button>{`Rating: ${movie.vote_average}`}</Button>
                 <Button>{movie.adult ? "PG-21": "PG-14"}</Button>
 
             </Card.Footer>
-        </Card.Root>
+        </MotionCard>
     )
 }
 
 export default MovieCard
+
+
+
