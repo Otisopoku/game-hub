aee69423784a6370f6e0918ef4794e1337b65ba1
Otis Opoku <otisopoku30@gmail.com>
2025-08-31 15:43:05 +0000

    add overlay to display movie description when movie card is hovered

--- src/components/created_components/MovieCard.tsx
+++ src/components/created_components/MovieCard.tsx
@@ -1,84 +1,86 @@
-
-import {motion} from "motion/react"
-import { Movie } from "@/hooks/useMovies"
-import { Button, Card, Image, Text } from "@chakra-ui/react"
+import { hover, motion } from "motion/react";
+import { Movie } from "@/hooks/useMovies";
+import { Box, Button, Card, Image, Text } from "@chakra-ui/react";
 import { Tooltip } from "@/components/ui/tooltip";
 import { SiPrimevideo } from "react-icons/si";
+import { useState } from "react";
 
-interface Props{
-    movie: Movie
+interface Props {
+  movie: Movie;
 }
 
-const descriptionLimit = 200; 
+const descriptionLimit = 200;
 
 const shortenDescription = (description: string) => {
-    if (description.length > descriptionLimit){
-        return description.slice(0, descriptionLimit);
-    }
-    return description;
-
-}
+  if (description.length > descriptionLimit) {
+    return description.slice(0, descriptionLimit);
+  }
+  return description;
+};
 
 const imageBaseUrl = "https://image.tmdb.org/t/p/w300";
 
 const getBackdropUrl = (backDropPath: string) => {
-    return `${imageBaseUrl}${backDropPath}`
-}
+  return `${imageBaseUrl}${backDropPath}`;
+};
 
 const MotionCard = motion.create(Card.Root);
 
-const MovieCard = ({movie}: Props) => {
-    const image_src = getBackdropUrl(movie.backdrop_path);
-    const isLong = movie.overview.length > descriptionLimit;
-    return (
-        
-        <MotionCard
-            maxW = "2xl"
-            overflow="hidden"
-            borderRadius= "4xl"
-            boxShadow="md"
-            whileHover={{
-                scale: 1.1,
-                transition: { duration: 0.2 },
-                zIndex: 1
-            }}
-            whileTap={{
-                scale: 0.8,
-            }}
-            transition={{
-                type: "spring",
-                stiffness: 400,
-                damping: 20,
-            }}
-            >
-            <Image src={image_src} width="100%"/>
-            <Card.Body>
-                <Card.Title>{movie.title}</Card.Title>
+const MovieCard = ({ movie }: Props) => {
+  const image_src = getBackdropUrl(movie.backdrop_path);
+  const isLong = movie.overview.length > descriptionLimit;
+  const [hovered, setHovered] = useState(false);
+  return (
+    <MotionCard
+      maxW="2xl"
+      overflow="hidden"
+      borderRadius="4xl"
+      boxShadow="md"
+      whileHover={{
+        scale: 1.1,
+        transition: { duration: 0.2 },
+        zIndex: 1,
+      }}
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+      whileTap={{
+        scale: 0.8,
+      }}
+      transition={{
+        type: "spring",
+        stiffness: 400,
+        damping: 20,
+      }}
+    >
+      <Image src={image_src} width="100%" />
+      <Card.Body>
+        <Card.Title>{movie.title}</Card.Title>
+      </Card.Body>
+      <Card.Footer>
+        <Button>{`Rating: ${movie.vote_average}`}</Button>
+        <Button>{movie.adult ? "PG-21" : "PG-14"}</Button>
+      </Card.Footer>
 
-                {
-                    isLong ? (
-                        <Text>
-                            {shortenDescription(movie.overview)}
-                            <Tooltip content={movie.overview} positioning = {{placement: "top"}}>
-                                <Button size = "xs" variant="ghost" p={1} ><Text fontSize="2xl">💡</Text></Button>
-                            </Tooltip>
-                        </Text>
-                    ): (
-                        <Text>{movie.overview}</Text>
-                    )
-                }
-                <SiPrimevideo size="50px"/>
-            </Card.Body>
-            <Card.Footer>
-                <Button>{`Rating: ${movie.vote_average}`}</Button>
-                <Button>{movie.adult ? "PG-21": "PG-14"}</Button>
+      <Box
+        position="absolute"
+        inset={0}
+        bg="rgba(0, 0, 0, 0, 0.85)"
+        color="white"
+        opacity={0}
+        p={4}
+        transition="opacity 0.25s ease"
+        _hover={{ opacity: 1 }}
+        borderRadius="xl"
+        overflow="auto"
+      >
+        {hovered && (
+          <Text mt={2} fontSize="sm">
+            {movie.overview}
+          </Text>
+        )}
+      </Box>
+    </MotionCard>
+  );
+};
 
-            </Card.Footer>
-        </MotionCard>
-    )
-}
-
-export default MovieCard
-
-
-
+export default MovieCard;
--- src/components/created_components/MovieGrid.tsx
+++ src/components/created_components/MovieGrid.tsx
@@ -1,21 +1,24 @@
-    import UseMovies from "@/hooks/useMovies"; 
-    import { SimpleGrid, Text } from "@chakra-ui/react";
-    import MovieCard from "./MovieCard";
-    
-    const MovieGrid = () => {
+import UseMovies from "@/hooks/useMovies";
+import { SimpleGrid, Text } from "@chakra-ui/react";
+import MovieCard from "./MovieCard";
+import { useState } from "react";
 
-        const {movies, error} = UseMovies(); 
-        return (
-        <>
-            {error && <Text>{error}</Text>}
-            <SimpleGrid columns={{sm: 1, md: 2, lg: 3, xl: 4}} padding="10px" gap="10px">
-                {movies.map(movie => <MovieCard movie = {movie} key={movie.id} />)}
-            </SimpleGrid>
-        </>
-        )
-    }
+const MovieGrid = () => {
+  const { movies, error } = UseMovies();
+  return (
+    <>
+      {error && <Text>{error}</Text>}
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+        padding="10px"
+        gap="10px"
+      >
+        {movies.map((movie) => (
+          <MovieCard movie={movie} key={movie.id} />
+        ))}
+      </SimpleGrid>
+    </>
+  );
+};
 
-    
-
-
-export default MovieGrid; 
+export default MovieGrid;
