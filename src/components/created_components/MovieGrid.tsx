2478047df9ae6d01b397fac2a8b46734d3577669
Otis Opoku <otisopoku30@gmail.com>
2025-09-06 23:49:36 +0000

    add loading state, and render skeleton while loading

--- src/components/created_components/MovieGrid.tsx
+++ src/components/created_components/MovieGrid.tsx
@@ -2,9 +2,11 @@ import UseMovies from "@/hooks/useMovies";
 import { SimpleGrid, Text } from "@chakra-ui/react";
 import MovieCard from "./MovieCard";
 import { useState } from "react";
+import MovieCardSkeleton from "./MovieCardSkeleton";
 
 const MovieGrid = () => {
-  const { movies, error } = UseMovies();
+  const { movies, error, isLoading } = UseMovies();
+  const skeletons = [1, 2, 3, 4, 5, 6];
   return (
     <>
       {error && <Text>{error}</Text>}
@@ -13,6 +15,8 @@ const MovieGrid = () => {
         padding="10px"
         gap="10px"
       >
+        {isLoading &&
+          skeletons.map((skeleton) => <MovieCardSkeleton key={skeleton} />)}
         {movies.map((movie) => (
           <MovieCard movie={movie} key={movie.id} />
         ))}
--- src/hooks/useMovies.ts
+++ src/hooks/useMovies.ts
@@ -1,44 +1,52 @@
 import apiClient from "@/services/api-client";
 import { CanceledError } from "axios";
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
 export interface Movie {
-    id: number; 
-    title: string; 
-    release_date: string; 
-    vote_average: number;
-    backdrop_path: string;
-    adult: boolean; 
-    genre_id: number;
-    overview: string
-
+  id: number;
+  title: string;
+  release_date: string;
+  vote_average: number;
+  backdrop_path: string;
+  adult: boolean;
+  genre_id: number;
+  overview: string;
 }
 
 export interface FetchPopularMoviesResponse {
-    page: number;
-    results: Movie[];
+  page: number;
+  results: Movie[];
 }
 
 const UseMovies = () => {
-    const [movies, setMovies] = useState<Movie []>([]); 
-    const [error, setError] = useState(''); 
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
-    useEffect(() => {
+  useEffect(() => {
+    const controller = new AbortController();
+    {
+      /** Handles the case when the user cancels the request */
+    }
+    setLoading(true);
+    apiClient
+      .get<FetchPopularMoviesResponse>("/popular", {
+        signal: controller.signal,
+      })
+      .then((res) => {
+        setMovies(res.data.results);
+        setLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setLoading(false);
+      });
 
-        const controller = new AbortController(); {/** Handles the case when the user cancels the request */}
-        apiClient.get<FetchPopularMoviesResponse>('/popular', {signal: controller.signal })
-        .then(res => setMovies(res.data.results))
-        .catch(err => {
-            if (err instanceof CanceledError) return; 
-            setError(err.message)
-            
-        })
+    return () => controller.abort();
+  }, []);
 
-        return () => controller.abort(); 
-    }, [])
-
-    return {movies, error}
-
-}
+  return { movies, error, isLoading };
+};
 
 export default UseMovies;
