282af5ede1996a1895f1872b6683b86ed3d7a93d
Otis Opoku <otisopoku30@gmail.com>
2025-09-07 23:04:11 +0000

    add a spinner

--- src/components/created_components/GenreList.tsx
+++ src/components/created_components/GenreList.tsx
@@ -1,8 +1,17 @@
 import useGenres, { Genre } from "@/hooks/useGenres";
 import GenreButton from "./GenreButton";
+import { Spinner, VStack, Text } from "@chakra-ui/react";
 
 const GenreList = () => {
-  const { genres } = useGenres();
+  const { genres, isLoading } = useGenres();
+
+  if (isLoading)
+    return (
+      <VStack colorPalette="teal">
+        <Spinner color="colorPalette.600" />
+        <Text color="colorPalette.600">Loading...</Text>
+      </VStack>
+    );
 
   return (
     <ul>
