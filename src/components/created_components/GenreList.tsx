541ccdeb9d9f4e7b4bf3a3c36bef51b6f9ea08c6
Otis Opoku <otisopoku30@gmail.com>
2025-09-07 23:15:38 +0000

    add an error state

--- src/components/created_components/GenreList.tsx
+++ src/components/created_components/GenreList.tsx
@@ -3,15 +3,18 @@ import GenreButton from "./GenreButton";
 import { Spinner, VStack, Text } from "@chakra-ui/react";
 
 const GenreList = () => {
-  const { genres, isLoading } = useGenres();
+  const { genres, isLoading, error } = useGenres();
 
-  if (isLoading)
+  if (error) {
+    return <Text>Sorry there was an error fetching genres {error}</Text>;
+  } else if (isLoading) {
     return (
       <VStack colorPalette="teal">
         <Spinner color="colorPalette.600" />
         <Text color="colorPalette.600">Loading...</Text>
       </VStack>
     );
+  }
 
   return (
     <ul>
